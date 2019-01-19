cb5b9520a842bfa623af3b78e361c34924cdd76b
James Robertson <15847227+jerobertson@users.noreply.github.com>
2019-01-19 20:32:54 +0000

    Initial commit.

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-  $server = "jerobertson.co.uk";
-  $username = "jerobert_master";
-  $password = "rRe!LX\M7Ljx}r4t+2g\p";
-  $dbname = "jerobert_jerobertson_co_uk_zmb";
+  $server = "127.0.0.1";
+  $username = "root";
+  $password = "root";
+  $dbname = "zmb";
   $conn = new mysqli($server, $username, $password, $dbname);
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
